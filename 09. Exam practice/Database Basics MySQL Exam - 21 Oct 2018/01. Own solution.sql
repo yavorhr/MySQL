0b7f1ba824321820eae476f6e489ce9269f9ce3e
wolfie <yavorhristozov@gmail.com>
2024-05-13 15:08:31 +0300

    14. Extract the shortest journey

--- 09. Exam practice/Database Basics MySQL Exam - 21 Oct 2018/01. Own solution.sql
+++ 09. Exam practice/Database Basics MySQL Exam - 21 Oct 2018/01. Own solution.sql
@@ -209,6 +209,18 @@ ON p.`id` = sp.`planet_id`
 ORDER BY (DATEDIFF(j.`journey_end`,j.`journey_start` )) ASC
 LIMIT 1;
 
- 
+# 14. Extract the shortest journey
+
+SELECT tc.`job_during_journey`
+FROM `travel_cards` as tc
+WHERE tc.`journey_id` =  (
+  SELECT j.`id`
+  FROM `journeys` as j
+  ORDER BY DATEDIFF(j.journey_end, j.journey_start) DESC
+  LIMIT 1
+)
+GROUP BY tc.`job_during_journey`
+ORDER BY COUNT(tc.`job_during_journey`)
+LIMIT 1;
  
 
