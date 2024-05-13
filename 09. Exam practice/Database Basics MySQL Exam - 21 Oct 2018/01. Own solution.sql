d2dccc5d4f1f2e8b44af9a35b65612e828ff5c30
wolfie <yavorhristozov@gmail.com>
2024-05-13 10:49:30 +0300

    07. Extract all pilots

--- 09. Exam practice/Database Basics MySQL Exam - 21 Oct 2018/01. Own solution.sql
+++ 09. Exam practice/Database Basics MySQL Exam - 21 Oct 2018/01. Own solution.sql
@@ -127,5 +127,16 @@ WHERE
     j.`purpose` = 'Military'
 ORDER BY j.`journey_start` ASC;
 
+# --- 07.	Extract all pilots
 
+SELECT 
+    c.`id`,
+    CONCAT_WS(' ', c.`first_name`, c.`last_name`) AS `full_name`
+FROM
+    `colonists` AS c
+        JOIN
+    `travel_cards` AS tc ON tc.`colonist_id` = c.`id`
+WHERE
+    tc.`job_during_journey` = 'Pilot'
+ORDER BY c.`id` ASC;
 
