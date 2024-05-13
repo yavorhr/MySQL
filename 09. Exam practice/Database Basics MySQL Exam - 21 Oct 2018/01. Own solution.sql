3510585ffd153f06954fc7dc770aff3493673cba
wolfie <yavorhristozov@gmail.com>
2024-05-13 12:03:49 +0300

    11. Extract all educational mission planets and spaceports

--- 09. Exam practice/Database Basics MySQL Exam - 21 Oct 2018/01. Own solution.sql
+++ 09. Exam practice/Database Basics MySQL Exam - 21 Oct 2018/01. Own solution.sql
@@ -166,13 +166,26 @@ LIMIT 1;
 # --- 10.Extract spaceships with pilots younger than 30 years
 
 SELECT s.`name`, s.`manufacturer`, tc.`id`
-FROM `spaceships` AS s
-JOIN `journeys` AS j
+FROM `spaceships` as s
+JOIN `journeys` as j
 ON j.`spaceship_id` = s.`id`
-JOIN `travel_cards` AS tc
+JOIN `travel_cards` as tc
 ON tc.`journey_id` = j.`id`
 JOIN `colonists` as c
 ON c.`id` = tc.`colonist_id`
 WHERE tc.`job_during_journey` = 'Pilot' AND YEAR(c.`birth_date`) > YEAR(DATE_SUB('2019-01-01', INTERVAL 30 YEAR))
 ORDER BY s.`name`;
+
+# --- 11. Extract all educational mission planets and spaceports
+
+SELECT p.`name` as `planet_name`, sp.`name` as `spaceport_name`, j.`id`
+FROM `planets` as p
+JOIN `spaceports` as sp
+ON p.`id` = sp.`planet_id`
+JOIN `journeys` as j
+ON j.`destination_spaceport_id` = sp.`id`
+WHERE j.`purpose` = 'Educational'
+ORDER BY sp.`name` DESC;
+
+ 
 
